9bb592906724396872a539a36d70d6674963b465
YALLICO OWNER <yallico2024@gmail.com>
2026-08-01 08:00:52 -0500

    Crear _love.js

---
+++ plugins/_love.js
@@ -0,0 +1,53 @@
+let handler = async (m, { conn, command }) => {
+  let who = m.mentionedJid && m.mentionedJid[0]
+        ? m.mentionedJid[0]
+          : m.quoted
+        ? m.quoted.sender
+          : m.sender;
+
+  let yo = m.sender
+  let nameYo = await conn.getName(yo);
+  let nameUser = await conn.getName(who);
+  let porcentaje = Math.floor(Math.random() * 101);
+
+  if(command == 'love'){
+    let frase = porcentaje < 30
+    ? '🌸 *NOS CUIDAMOS COMO AMIGOS*'
+      : porcentaje < 60
+    ? '💌 *HAY ALGO BONITO ENTRE USTEDES*'
+      : porcentaje < 85
+    ? '🤍 *SE HACEN MUCHO BIEN JUNTOS*'
+      : '💍 *ESTÁN HECHOS EL UNO PARA EL OTRO*'
+
+    let detallito = porcentaje < 30
+    ? 'A veces el mejor amor es el de amigos 💫'
+      : porcentaje < 60
+    ? 'Denle tiempo... las cosas bonitas florecen lento 🥺'
+      : porcentaje < 85
+    ? 'Se nota que se quieren mucho. Cuídense 🤍'
+      : 'Prométanse ser felices juntos siempre ✨'
+
+    await conn.sendMessage(m.chat, {
+      text: `ᯇ 💕 𝗖𝗨𝗣𝗜𝗗𝗢 𝗕𝗢𝗧 💕 ୧
+
+꒰ ◞⁺⊹ ．*${nameYo}* y *${nameUser}*
+
+*Compatibilidad:* *${porcentaje}%* 💘
+
+${frase}
+
+──愛 *𝗠𝗘𝗡𝗦𝗔𝗝𝗜𝗧𝗢* ╏ 🕊️
+"${detallito}"
+
+> *Que el amor los encuentre bonito* 🌙`,
+      mentions: [yo, who]
+    }, {quoted: m})
+  }
+}
+
+handler.help = ['love *@usuario*']
+handler.tags = ['love']
+handler.command = /^(love|amor|compatibilidad)$/i
+handler.group = true
+
+export default handler
