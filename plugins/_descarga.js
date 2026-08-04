083f71a153444f6a8e9682ca8e1df060c844f9df
YALLICO OWNER <yallico2024@gmail.com>
2026-08-03 19:37:34 -0500

    Actualizar _descarga.js

--- plugins/_descarga.js
+++ plugins/_descarga.js
@@ -310,7 +310,7 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
 
 .⃟𖥔 ݁. 𖦹˙— \`\`𝐏𝐫𝐞𝐦\`\` —˙𖦹.⚠️꒷
 
- ⤷ ┇ 𝗘𝗥𝗢𝗥 𝗗𝗘 𝗦𝗜𝗦𝗧𝗘𝗠𝗔 ：✿ 。
+ ⤷ ┇ 𝗘𝗥𝗥𝗢𝗥 𝗗𝗘 𝗦𝗜𝗦𝗧𝗘𝗠𝗔 ：✿ 。
 
 ──愛 *FALLA DETECTADA* ╏ ❄️
 ⚠️ ➛ Detalle: ${msgs[e] || 'ERROR INESPERADO'}
